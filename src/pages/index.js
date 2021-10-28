119c14171d42248897108a8455804306193d17cf
mklr-nl <info@mklr.nl>
2021-10-28 16:10:29 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,7 @@
 import * as React from "react"
 import Layout from "../components/Layout"
-import Seo from "../components/Seo"
+// import Seo from "../components/Seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="YOUR" />
-  </Layout>
-)
+const IndexPage = () => <Layout>{/* <Seo title="YOUR" /> */}</Layout>
 
 export default IndexPage
